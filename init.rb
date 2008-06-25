0999c62949b59c449fc33f9e74050154dae28a44
lukeredpath <lukeredpath@53438b35-87ee-0310-8825-c8c94c0097b6>
2008-06-25 13:46:52 +0000

    We cant reliably auto include the controller mixin into ApplicationController as it may not have been defined
    git-svn-id: http://svn/repos/plugins/simple_config@11157 53438b35-87ee-0310-8825-c8c94c0097b6

--- init.rb
+++ init.rb
@@ -2,5 +2,4 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), *%w[lib])
 
 require 'rails_compatibility' 
 require 'simple_config'
-
-ApplicationController.send(:include, SimpleConfig::ControllerMixin)
+require 'controller_mixin'
