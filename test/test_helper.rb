7d93f658585c972e2a8e2da0e904c59d7b9e74de
Luke Redpath <contact@lukeredpath.co.uk>
2008-11-21 14:23:59 +0000

    Require at least ActiveSupport 2.0 for running the tests (I'm not sure exactly when Hash#except was added

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), *%w[.. lib])
 
 unless defined?(ActiveSupport) # we aren't in a rails environment
   require 'rubygems'
+  gem 'activesupport', '>=2.0.0'
   require 'active_support'
 end
 
