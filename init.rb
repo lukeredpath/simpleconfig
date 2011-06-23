64ee4e9488264e630ab7e249006a4bdcdd7b91f9
Luke Redpath <luke@lukeredpath.co.uk>
2011-06-23 17:07:36 +0100

    Rails compatibility patch is no longer needed (closes #12)

--- init.rb
+++ init.rb
@@ -1,5 +1,4 @@
 $LOAD_PATH << File.join(File.dirname(__FILE__), 'lib')
 
-require 'rails_compatibility' 
 require 'simple_config'
 require 'simple_config/controller_mixin'
--- lib/rails_compatibility.rb
+++
@@ -1,7 +0,0 @@
-unless Rails.respond_to?(:env)
-  Rails.instance_eval do
-    def env
-      RAILS_ENV
-    end
-  end
-end
